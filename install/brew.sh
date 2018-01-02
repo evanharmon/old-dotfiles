939f63e622b7728d218508fe8f6eae32c01cc88f
Evan Harmon <evan.p.harmon@gmail.com>
2018-01-01 17:03:21 -0700

    feat(install): push up installs

---
+++ install/brew.sh
@@ -0,0 +1,17 @@
+# HOMEBREW STAR RUNNER
+brew install zsh tmux neovim/neovim/neovim python3 reattach-to-user-namespace
+brew tap caskroom/cask
+brew tap caskroom/fonts
+brew cask install iterm2
+brew install fd fzf editorconfig git-flow jq
+brew install --HEAD universal-ctags/universal-ctags/universal-ctags
+## COMPILED LANGS TOOLS
+brew install criterion
+brew install delve
+
+## JS
+brew install yarn
+brew cask install graphiql
+
+## UTILS
+brew cask install keycastr
