402d8fc615d8f4a73deed4a7bac160dfa30d0226
evan <evan@evans-MacBook-Pro.local>
2018-08-27 18:18:30 -0400

    adjust readme and base.sh

--- README.md
+++ README.md
@@ -10,7 +10,7 @@ With this power comes responsibility. NEVER BLANKET ADD aka `dotfiles add .`
 open terminal
 `$ cd $HOME`
 
-## Step 1: GIT repo
+## Step 1: GIT repo / ZSH
 `$ git init --bare $HOME/.dotfiles`
 If prompted to, install xcode developer tools
 
@@ -21,11 +21,20 @@ $ dotfiles remote add origin https://github.com/evanharmon/dotfiles.git
 $ dotfiles pull origin master
 ```
 
-## Step 2: Tools
-### Sudo password will be requested as user input
+## Step 2: Install Tools
+### Sudo password will be requested as user input several times
 ```
 $ chmod -R a+x ./install/
 $ ./install/brew.sh
 ```
-## Step 3: ZSH
+
+## Step 3: Setup iTerm
+### AWS Git https git credentials required
+In the `General` tab of iTerm preferences, check the box labelled
+`load preferences from a custom folder or URL` and browse for the folder
+`$HOME/iterm2`
+Check the box below it marked `Save changes to folder when iTerm2 quits`.
+Restart iTerm
+
+## Step 4: ZSH
 `$ source $HOME/.zshrc`
--- install/base.sh
+++ install/base.sh
@@ -20,14 +20,18 @@ fi
 
 if ! which python3; then
 	brew install python3 reattach-to-user-namespace
+else
+	echo "python 3 already installed"
 fi
 
 if ! which nvim; then
+	brew tap neovim/neovim
+	brew install --HEAD neovim
+else 
 	echo "neovim already installed"
 fi
 
 brew tap caskroom/cask
-brew tap caskroom/fonts
 if brew cask ls --versions iterm2 > /dev/null; then
 	echo "iterm2 already installed"
 else
@@ -45,4 +49,18 @@ if ! which fzf; then
 	echo "fzf already installed"
 else
 	brew install fzf
+fi
+
+if [ ! -f $HOME/.config/hss-fonts/README.md ]; then
+	echo "installing fonts"
+	git clone https://git-codecommit.us-east-1.amazonaws.com/v1/repos/hss-fonts $HOME/.config/hss-fonts
+	cp $HOME/.config/hss-fonts/*.otf $HOME/Library/Fonts/
+fi
+
+if git config --global user.name != "Evan Harmon"; then
+	git config --global user.name "Evan Harmon"
+fi
+
+if git config --global user.email != "evan.p.harmon@gmail.com"; then
+	git config --global user.email "evan.p.harmon@gmail.com"
 fi
