8d566bf189d0bdde95e17a57fc2b67b6c24f233d
Evan Harmon <evan.p.harmon@gmail.com>
2019-07-21 14:11:21 -0400

    adjust k8s for alias on kubernetes in zsh

--- .config/zsh/k8s.zsh
+++ .config/zsh/k8s.zsh
@@ -3,11 +3,11 @@
 # https://raw.githubusercontent.com/ahmetb/kubectl-alias/master/.kubectl_aliases
 # Defines Kubernetes aliases.
 
-alias k8='kubectl'
-alias k8a='kubectl apply'
-alias k8dp='kubectl describe pods'
-alias k8gp='kubectl get pods'
-alias k8ddep='kubectl describe deployment'
-alias k8gdep='kubectl get deployment'
-alias k8dsvc='kubectl describe service'
-alias k8gsvc='kubectl get service'
+alias k8s='kubectl'
+alias k8sa='kubectl apply'
+alias k8sdp='kubectl describe pods'
+alias k8sgp='kubectl get pods'
+alias k8sddep='kubectl describe deployment'
+alias k8sgdep='kubectl get deployment'
+alias k8sdsvc='kubectl describe service'
+alias k8sgsvc='kubectl get service'
